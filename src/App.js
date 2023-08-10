6d60c4148657b83ff4f8ac7bb62865c8f89cf956
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-10 20:48:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from "react";
 import Home from "./screens/Home";
 import{BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <Router>
       <div>
-       
+       <Navbar />
       </div>
     </Router>
   );

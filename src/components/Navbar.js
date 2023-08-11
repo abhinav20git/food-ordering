580051fb378f3dd05dfda64a7a75b0a1822cacb6
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-11 13:23:06 +0530

    Created card and Navbar using bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 import {Link} from 'react-router-dom';
+import Logo from './Logo2.png';
+
 
 export default function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container-fluid">
-              <Link className="navbar-brand" to="/">Navbar</Link>
+              <Link className="navbar-brand" to="/"><img src={Logo} alt="logo" style={{width:"10rem"}}/></Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav">
                       <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <Link className="nav-link active fst-italic" aria-current="page" to="/">Home</Link>
                       </li>
                       <li className="nav-item">
-                        <Link className="nav-link" to="/">Features</Link>
+                        <Link className="nav-link active fst-italic" to="/login">Login</Link>
                       </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Pricing</Link>
-                      </li>
+                      
                   </ul>
               </div>
           </div>

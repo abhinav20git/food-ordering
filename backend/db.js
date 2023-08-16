2bd6fae1c734a538fa93d5b5df6e11c4175ab6a7
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-16 13:15:31 +0530

    Update db.js
    fetched data from database

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Fiesta-La-Blanc-menu:9828206Uk@clustermenu.rkgdjtl.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://Fiesta-La-Blanc-menu:9828206Uk@clustermenu.rkgdjtl.mongodb.net/ClusterMenu?retryWrites=true&w=majority'
 
 const mongoDB =async() =>{
 
-    await mongoose.connect(mongoURI,{useNewUrlParser: true},(err, result)=>{
+    await mongoose.connect(mongoURI,{useNewUrlParser: true},async(err, result)=>{
     if (err) console.log("---", err)
     else{
         console.log("connected");
+        const fetched_data = await mongoose.connection.db.collection("food_items")
+        fetched_data.find({}).toArray(function(err,data){
+            if(err) console.log(err);
+            else console.log(data)
+        })
     }
         
     });

1e34de161fbaeb50c955733f372952d8b18a63a4
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-17 20:26:09 +0530

    Added Login functionality
    Now website is able to fetch data from mongodb servers and rectify whether the email and password entered are present in the database or not.

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-router.post("/creatuser",
+router.post("/creatuser", [
     body('email').isEmail(),
     body('name').isLength({ min: 6 }),
-    body('password','Incorrect Password').isLength({ min: 5 }),
+    body('password', 'Incorrect Password').isLength({ min: 5 })],
     async (req, res) => {
 
         const errors = validationResult(req);
@@ -22,7 +22,35 @@ router.post("/creatuser",
                 email: req.body.email,
                 location: req.body.location
             }).then(res.json({ success: true }));
-            
+
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false });
+
+        }
+
+    })
+
+router.post("/loginuser", [
+    body('email').isEmail(),
+    body('password', 'Incorrect Password').isLength({ min: 5 })], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+
+
+        }
+        let email = req.body.email;
+        try {
+            let userData = await User.findOne({ email });
+            if (!userData) {
+                return res.status(400).json({ errors: "Try logging in with correct Email" })
+            }
+            if (req.body.password !== userData.password) {
+                return res.status(400).json({ errors: "Try logging in with correct Password" })
+            }
+            return res.json({ success: true })
+
         } catch (error) {
             console.log(error)
             res.json({ success: false });

08fb292f71e9bc7f1ed31c222c712ae209280866
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-19 12:41:11 +0530

    Update Home.js
    Moved the carousel code to Home.js, as carousal is being used only once and to implement search bar the code needs to be in same directory as the carousal.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
-import Carousal from '../components/Carousal'
+
 
 
 export default function Home() {
 
-
+  const [search, setSearch] = useState('');
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
@@ -39,7 +39,38 @@ export default function Home() {
   return (
     <div>
       <div><Navbar /></div>
-      <div><Carousal /></div>
+      <div>
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: "contain !important" }}>
+          <div className="carousel-inner" id="carousel">
+            <div className='carousel-caption' style={{ "zIndex": "10" }}>
+              <div className="d-flex justify-conten-center">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button>
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img src="https://images.unsplash.com/photo-1571091655789-405eb7a3a3a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" className="d-block w-100 h-50" style={{ filter: "brightness(30%)" }} alt="burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://cdn.pixabay.com/photo/2020/05/17/04/22/pizza-5179939_640.jpg" className="d-block w-100 h-50" style={{ filter: "brightness(30%)" }} alt="pizza" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://images.alphacoders.com/130/1308370.jpg" className="d-block w-100 h-50" style={{ filter: "brightness(30%)" }} alt="pasta" />
+            </div>
+
+
+
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
       <div className='container'>
         {
           foodCat !== []
@@ -55,10 +86,10 @@ export default function Home() {
                     return (
                       <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
 
-                        <Card foodName = {filterItems.name}
-                        options = {filterItems.options[0]}
-                        imgSrc = {filterItems.img}                        
-                        
+                        <Card foodName={filterItems.name}
+                          options={filterItems.options[0]}
+                          imgSrc={filterItems.img}
+
                         ></Card>
 
                       </div>
@@ -73,7 +104,7 @@ export default function Home() {
             })
             : ""
         }
-    
+
 
       </div>
 

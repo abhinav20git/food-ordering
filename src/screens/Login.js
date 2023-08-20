644052c65e277f6c8cf78d9ff94f5a2778fcefcd
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-20 11:51:30 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,3 +1,4 @@
+import { Button } from 'bootstrap/dist/js/bootstrap.bundle';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -35,21 +36,22 @@ let navigate = useNavigate();
   }
   return (
     <div>
-      <div className="container">
+      <div className="container mt-5 pt-5">
         <form onSubmit={handleSubmit}>
 
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <div className="mb-3 row col-12 col-sm-8 col-md-6 m-auto">
+            <label htmlFor="exampleInputEmail1" className="form-label h1">Email address</label>
             <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <div className="mb-3 row col-12 col-sm-8 col-md-6 m-auto">
+            <label htmlFor="exampleInputPassword1" className="form-label h1">Password</label>
             <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
           </div>
-
-          <button type="submit" className="btn btn-primary">Submit</button>
-          <Link to="/creatuser" className='m-3 btn btn-danger'>I'm a new user</Link>
+          <div className="d-grid">
+          <button type="submit" className="btn btn-primary text-color-white bg-black btn-block text-center btn-outline-info ">Submit</button>
+          <Link to="/creatuser" className='m-3 btn btn-danger btn-block text-center btn-outline-warning'> new user</Link>
+          </div>
         </form>
       </div>
 
